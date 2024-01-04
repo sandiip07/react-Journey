d0f1667927a17183c3f795200dc7a1b74609fd16
sandiip07 <sandiip2021@gmail.com>
2024-01-04 00:54:13 -0800

    password generator

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState();
+
+
 
   return (
     <>
-      hyr
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button className="outline-none rounded-full text-white back px-4 py-2"
+            style={{backgroundColor:"red"}}
+            onClick={() => {setColor("red")}}
+            >Red</button>
+            <button className="outline-none rounded-full text-white back px-4 py-2"
+            style={{backgroundColor:"green"}}
+            onClick={() => {setColor("green")}}
+            >Green</button>
+            <button className="outline-none rounded-full text-white back px-4 py-2"
+            style={{backgroundColor:"yellow"}}
+            onClick={() => {setColor("yellow")}}
+            >Yellow</button>
+            <button className="outline-none rounded-full text-white back px-4 py-2"
+            style={{backgroundColor:"purple"}}
+            onClick={() => {setColor("purple")}}
+            >Purple</button>
+            <button className="outline-none rounded-full text-white back px-4 py-2"
+            style={{backgroundColor:"blue"}}
+            onClick={() => {setColor("blue")}}
+            >Blue</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,101 @@
+import { useCallback, useState, useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setpassword] = useState("");
+
+  // refuse Hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let strData = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if (numberAllowed) {
+      strData = strData + "0123456789";
+    }
+    if (charAllowed) {
+      strData = strData + "!@#$%^&*()~";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * strData.length + 1);
+      pass += strData.charAt(char);
+    }
+    setpassword(pass);
+  }, [length, numberAllowed, charAllowed, setpassword]);
+
+  const copyToClipBoard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,5);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, charAllowed, numberAllowed, passwordGenerator]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto rounded-lg px-4 py-8  text-white bg-gray-800 ">
+        Password Generator
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 text-black"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyToClipBoard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 "
+          >
+            Copy
+          </button>
+        </div>
+        <div className="text-sm flex gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={5}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label className="text-yellow">Length:{length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkBox"
+              defaultChecked={numberAllowed}
+              onClick={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label>Number</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkBox"
+              defaultChecked={charAllowed}
+              onClick={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label>Character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
